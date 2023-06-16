8046cc51d65b25d5ec189546587a1a5d028ddcb6
bombag456 <bombag456@gmail.com>
2023-06-16 17:15:34 +0600

    Initial Commit

---
+++ Namba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Namba
+//
+//  Created by Mac on 16/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
